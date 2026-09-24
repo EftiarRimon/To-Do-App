db4f9cdaf3d29c04fec52566da61d55fa2a866d8
EftiarRimon <bsse1356@iit.du.ac.bd~>
2026-09-25 01:31:58 +0600

    To Do App

--- backEnd/toDoList.js
+++ backEnd/toDoList.js
@@ -2,10 +2,17 @@ const taskInput=document.getElementById("taskInput")
 const TaskList = document.getElementById("TaskList");
 
 let tasks=[];
-function addTask(){
-    const text=taskInput.value;
 
+function addTask(){
+    const text=taskInput.value.trim();
+    
+    if(text==""){
+        alert("write something")
+    } else{
+    
     tasks.push({
+
+        
         id:Date.now(),
         text:text,
         completed:false
@@ -15,22 +22,76 @@ function addTask(){
     renderTasks();
 
 }
+}
 
- 
- function deleteTask(){
-    tasks.pop();
+
+function DeleteTask(id){
+  const updatedTask=tasks.filter(function(task){
+    return task.id !==id;
+  })
+  tasks=updatedTask;
+  renderTasks();
+}
+
+function EditTask(id){
+    const task=tasks.find(function(t){
+        return t.id===id;
+
+})
+   const newText=prompt("Edit Task:", task.text);
+        task.text=newText;
+        renderTasks();
+}
+
+function toggleComplete(id){
+    const task=tasks.find(function(t){
+        return t.id===id;
+    });
+
+    task.completed=!task.completed;
     renderTasks();
- }
+}
 
 function renderTasks(){
     TaskList.innerHTML=""
-    tasks.forEach(function showTask(task){
+    tasks.forEach(function showTask(task, index){
+
         const li=document.createElement("li");
-        li.textContent=task.text;
+        li.textContent="task"+(index+1)+":"+task.text;
+        if (task.completed) {
+            h1.style.textDecoration = "line-through";
+        } else {
+            li.style.textDecoration = "none";
+        }
+
         TaskList.appendChild(li);
+
+        const DltButton=document.createElement("Button");
+        DltButton.textContent="Delete"
+        li.appendChild(DltButton);
+        DltButton.addEventListener("click", function(){
+            DeleteTask(task.id)
+        })
+        const editButton=document.createElement("Button")
+        editButton.textContent="edit"
+        li.appendChild(editButton);
+        editButton.addEventListener("click",function(){
+            EditTask(task.id)
+        })
+
+        const checkBox=document.createElement("input");
+        checkBox.type="checkbox";
+        checkBox.checked=task.completed;
+
+        checkBox.addEventListener("click",function(){
+            toggleComplete(task.id);
+        });
+        li.appendChild(checkBox);
+
+        
+        
     });
 }
 document.getElementById("addTask").addEventListener("click",addTask);
-document.getElementById("dltButton").addEventListener("click",deleteTask);
 
 
